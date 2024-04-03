2cfc8a69f4e54760440fefb75f6a86e8ffc9f5d2
alejandroins25 <amartinezinstituto@gmail.com>
2024-04-03 20:06:43 +0200

    paso J

--- src/operacions/Factorial.java
+++ src/operacions/Factorial.java
@@ -2,18 +2,14 @@ package operacions;
 
 public class Factorial {
 
-	public double factorialIteratiu(int n) {
+	public double factorialRecursiu(int n) {
 		if (n < 0) {
-			throw new IllegalArgumentException();
+		throw new IllegalArgumentException();
 		}
 		if (n == 1 || n == 0) {
-			return 1;
+		return 1;
 		}
-
-		int aux = 1;
-		for (int i = 2; i <= n; i++) {
-			aux *= i;
+		Factorial fmenos1 = new Factorial();
+		return n * fmenos1.factorialRecursiu(n - 1);
 		}
-		return aux;
-	}
 }
